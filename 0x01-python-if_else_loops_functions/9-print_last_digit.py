0cece1f747e5d025c19022008a93fa7ff37343a0
Kamohelo99 <kamohelo0329@gmail.com>
2022-06-01 22:27:19 +0000

    modifed file to fit pycodestyle validation

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    if number < 0:  #difference in modulo between python and c
+    if number < 0:
         abs = number * -1
         digit = (abs % 10)
     else:
