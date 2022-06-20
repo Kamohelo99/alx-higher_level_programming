9a2b900f0a3a81ddb8439e9f303d9ae6f8d8bbe5
Kamohelo99 <kamohelo0329@gmail.com>
2022-06-20 21:00:38 +0000

    added sys library

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
+import sys
 
 def safe_function(fct, *args):
-    import sys
     try:
-        return (fct(*args))
-    except Exception as error:
-        print("Exception: {}".format(error), file=sys.stderr)
-        return None
+        res = fct(*args)
+        return res
+    except Exception as err:
+        sys.stdout.write("Exception: " + str(err) + "\n")
+        return
