4f4b6da0feb2f56ccb798a70c26129cf6efbe73a
Kamohelo99 <kamohelo0329@gmail.com>
2022-06-20 20:44:27 +0000

    a function that executes a function safely.
        Prototype: def safe_function(fct, *args):
        You can assume fct will be always a pointer to a function
        Returns the result of the function,
        Otherwise, returns None if something happens during the function and prints in stderr the error precede by Exception:
        You have to use try: / except:

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 
-def safe_print_integer_err(value):
+def safe_function(fct, *args):
     import sys
     try:
-        print("{:d}".format(value))
-        return True
+        return (fct(*args))
     except Exception as error:
         print("Exception: {}".format(error), file=sys.stderr)
-        return False
+        return None
