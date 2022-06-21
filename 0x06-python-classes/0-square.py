3a90d9bc2538b845fe94fea8610230d6da42a994
Kamohelo99 <kamohelo0329@gmail.com>
2022-06-21 20:53:58 +0000

    a class Square that defines a square by: (based on 0-square.py)
        Private instance attribute: size
        Instantiation with size (no type/value verification)
        You are not allowed to import any module

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-"""Defines a class Square"""
+""" Defines a class Square"""
 
 
 class Square:
+
     """An empty Square class"""
     pass
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""square class with a private method """
+
+
+class Square:
+
+    """Square class """
+
+    def __init__(self, size):
+        """
+        The __init__ method
+        """
+        self.__size = size
